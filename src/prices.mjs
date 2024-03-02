cd0cb665e4a03eb03ca2b51d25b23854445d3615
Mika Seppälä <mjseppala@gmail.com>
2024-03-02 23:49:33 +0200

    Add convertDate function.

--- src/prices.mjs
+++ src/prices.mjs
@@ -29,6 +29,14 @@ function createApp(database) {
     }
   }
 
+  function convertDate(date) {
+    if (date) {
+      return date.toTemporalInstant()
+      .toZonedDateTimeISO("UTC")
+      .toPlainDate();
+    }
+  }
+
   function calculateCost(age, type, date, baseCost) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
